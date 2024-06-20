1c9a18d7f00eca6e41bccb62cd10942083af0d3f
Daniel Perez <100069700+danielperezz@users.noreply.github.com>
2024-06-20 14:49:42 +0300

    Update llm2.py

--- AI-backend/llm2.py
+++ AI-backend/llm2.py
@@ -12,27 +12,32 @@ import os
 
 os.environ['OPENAI_API_KEY'] = 'sk-proj-lhrGw7CEUgR5WPe121plT3BlbkFJrbDuDxFBIgADbCxugJnF'
 os.environ['PINECONE_API_KEY'] = '39e6ca0c-4d12-419b-9623-d4e1c0f871ca'
-
 index_name = "writing-history"
 embeddings = OpenAIEmbeddings()
 
-# vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
 
-loader = TextLoader("history.txt")
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+def init_langchain():
+    pass
 
-vectorstore = PineconeVectorStore.from_documents(
-    docs,
-    index_name=index_name,
-    embedding=embeddings
-)
+def upload_doc(str_doc, user_id):
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents([str_doc])
+    vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
+    for i, doc in enumerate(docs):
+        doc.metadata = {
+            "user_id": user_id
+        }
+    vectorstore.add_documents(docs)
 
-# Convert documents to embeddings
-# texts = [doc for doc in documents]
-# metadatas = [{"source": doc.metadata["source"]} for doc in documents]
-# embeddings_vectors = [embeddings.embed(text) for text in texts]
+def generate_writing_prompt():
+    pass
+
+
+# loader = TextLoader("history.txt")
+# documents = loader.load()
+
+
+
 
 retriever = vectorstore.as_retriever()
 
@@ -43,5 +48,8 @@ llm = ChatOpenAI(model_name="gpt-4", temperature=0.5)
 chain = RetrievalQAWithSourcesChain.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
 
 # Query the chain
-answer = chain({"question": "give me a prompt to journal"}, return_only_outputs=True)
-print(answer)
+# answer = chain({"question": "give me a prompt to journal"}, return_only_outputs=True)
+# print(answer)
+
+def main():
+    print("hi")
