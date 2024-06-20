34ca3190438b7274e5a46dc342304fae9ad739b5
noyzini <102484852+noyzini@users.noreply.github.com>
2024-06-20 23:52:12 +0300

    add_ui

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home.js'
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-        <Home/>
+       <Home/>
     </div>
   );
 }
